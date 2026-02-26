6462da61f06cc07b900aabd6c22bb7f07e841657
Khodyrev Nikita <sigma29034@gmail.com>
2026-02-26 09:54:56 +0500

    Разработанная страница добавления и изменения состояний. Osennikov

---
+++ Pages/State/Add.xaml.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace VinylRecordsApplication.Pages.State
+{
+    /// <summary>
+    /// Логика взаимодействия для Add.xaml
+    /// </summary>
+    public partial class Add : Page
+    {
+        private Classes.State changeState;
+        public Add(Classes.State state)
+        {
+            InitializeComponent();
+            if (state != null)
+            {
+                this.changeState = state;
+                this.tbName.Text = state.Name;
+                this.tbSubname.Text = state.Subname;
+                this.tbDescription.Text = state.Description;
+
+                addBtn.Content = "Изменить";
+            }
+        }
+
+        private void AddState(object sender, RoutedEventArgs e)
+        {
+            if (!string.IsNullOrEmpty(tbName.Text))
+            {
+                if (!string.IsNullOrEmpty(tbSubname.Text))
+                {
+                    if (this.changeState == null)
+                    {
+                        Classes.State newState = new Classes.State()
+                        {
+                            Name = tbName.Text,
+                            Subname = tbSubname.Text,
+                            Description = tbDescription.Text
+                        };
+                        newState.Save();
+                        MessageBox.Show($"Состояние {newState.Name} успешно добавлено.", "Уведомление");
+                        MainWindow.mainWindow.OpenPage(new Pages.State.Add(newState));
+                    }
+                    else
+                    {
+                        changeState.Name = tbName.Text;
+                        changeState.Subname = tbSubname.Text;
+                        changeState.Description = tbDescription.Text;
+                        changeState.Save(true);
+                        MessageBox.Show($"Состояние {changeState.Name} успешно изменено.", "Уведомление");
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Пожалуйста, укажите сокращённое наименование состояния.", "Предупреждение");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Пожалуйста, укажите наименование состояния.", "Предупреждение");
+            }
+        }
+    }
+}
--- Pages/State/Elements/State.xaml.cs
+++ Pages/State/Elements/State.xaml.cs
@@ -20,9 +20,46 @@ namespace VinylRecordsApplication.Pages.State.Elements
     /// </summary>
     public partial class State : UserControl
     {
-        public State()
+        private Classes.State state;
+        private Pages.State.Main main;
+
+        public State(Classes.State state, Pages.State.Main main)
         {
             InitializeComponent();
+
+            this.state = state;
+            this.main = main;
+            tbName.Text = this.state.Name;
+            tbSubname.Text = this.state.Subname;
+            tbDescription.Text = this.state.Description;
+        }
+
+        private void EditState(object sender, RoutedEventArgs e)
+        {
+            MainWindow.mainWindow.OpenPage(new Pages.State.Add(state));
+        }
+
+        private void DeleteState(object sender, RoutedEventArgs e)
+        {
+            if (MessageBox.Show($"Удалить состояние: {this.state.Name}?",
+                                "Уведомление",
+                                MessageBoxButton.YesNo) == MessageBoxResult.Yes)
+            {
+                IEnumerable<Classes.Record> AllRecord = Classes.Record.AllRecords();
+                if (AllRecord.Where(x => x.IdState == state.Id).Count() > 0)
+                {
+                    MessageBox.Show($"Состояние {this.state.Name} невозможно удалить. " +
+                                   "Для начала удалите зависимости.",
+                                   "Уведомление");
+                }
+                else
+                {
+                    this.state.Delete();
+                    main.stateParent.Children.Remove(this);
+                    MessageBox.Show($"Состояние {this.state.Name} успешно удалено.",
+                                   "Уведомление");
+                }
+            }
         }
     }
 }
