9cadc1d7c70157db53b42d6421b36d784fdaad74
Khodyrev Nikita <sigma29034@gmail.com>
2026-02-26 11:03:29 +0500

    Разработка страницы для добавления и изменения поставщика. Osennikov

---
+++ Pages/Manufacturer/Add.xaml.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace VinylRecordsApplication.Pages.Manufacturer
+{
+    /// <summary>
+    /// Логика взаимодействия для Add.xaml
+    /// </summary>
+    public partial class Add : Page
+    {
+        public IEnumerable<Classes.Country> AllCountries = Classes.Country.AllCountries();
+        Classes.Manufacturer changeManufacturer;
+
+        public Add(Classes.Manufacturer changeManufacturer = null)
+        {
+            InitializeComponent();
+            foreach (var Country in AllCountries)
+                tbCountry.Items.Add(Country.Name);
+            if (AllCountries.Count() > 0)
+                // Выбираем первую страну
+                tbCountry.SelectedIndex = 0;
+            if (changeManufacturer != null)
+            {
+                this.changeManufacturer = changeManufacturer;
+                tbName.Text = changeManufacturer.Name;
+                tbPhone.Text = changeManufacturer.Phone;
+                tbEmail.Text = changeManufacturer.Mail;
+                tbCountry.SelectedIndex = AllCountries.ToList().FindIndex(x => x.Id == changeManufacturer.CountryCode);
+                addBtn.Content = "Изменить";
+            }
+        }
+
+        private void AddManufacturer(object sender, RoutedEventArgs e)
+        {
+            if (!String.IsNullOrEmpty(tbName.Text))
+                if (!String.IsNullOrEmpty(tbPhone.Text))
+                    if (!String.IsNullOrEmpty(tbEmail.Text))
+                        if (CorrectPhone(tbPhone.Text))
+                            if (CorrectEmail(tbEmail.Text))
+                            {
+                                if (changeManufacturer == null)
+                                {
+                                    Classes.Manufacturer manufacturer = new Classes.Manufacturer()
+                                    {
+                                        Name = tbName.Text,
+                                        Phone = tbPhone.Text,
+                                        Mail = tbEmail.Text,
+                                        CountryCode = AllCountries.Where(x => x.Name == tbCountry.SelectedItem.ToString()).First().Id
+                                    };
+                                    manufacturer.Save();
+                                    MessageBox.Show($"Поставщик {manufacturer.Name} успешно добавлен.", "Уведомление");
+                                    MainWindow.mainWindow.OpenPage(new Add(manufacturer));
+                                }
+                                else
+                                {
+                                    changeManufacturer.Name = tbName.Text;
+                                    changeManufacturer.Phone = tbPhone.Text;
+                                    changeManufacturer.Mail = tbEmail.Text;
+                                    changeManufacturer.CountryCode = AllCountries.Where(x => x.Name == tbCountry.SelectedItem.ToString()).First().Id;
+                                    changeManufacturer.Save(true);
+                                    MessageBox.Show($"Поставщик {changeManufacturer.Name} успешно изменён.", "Уведомление");
+                                }
+                            }
+                            else
+                                MessageBox.Show("Пожалуйста, укажите почту поставщика в формате xx@xx.xx.", "Предупреждение");
+                        else
+                            MessageBox.Show("Пожалуйста, укажите номер поставщика в формате 89000000000.", "Предупреждение");
+                    else
+                        MessageBox.Show("Пожалуйста, укажите почту поставщика.", "Предупреждение");
+                else
+                    MessageBox.Show("Пожалуйста, укажите телефон поставщика.", "Предупреждение");
+            else
+                MessageBox.Show("Пожалуйста, укажите наименование поставщика.", "Предупреждение");
+        }
+
+        public bool CorrectPhone(string Value)
+        {
+            string $Regex = "^8[0-9]{9}$";
+            Regex regex = new Regex($Regex);
+            MatchCollection matches = regex.Matches(Value);
+            return matches.Count > 0;
+        }
+
+        public bool CorrectEmail(string Value)
+        {
+            string $Regex = "[aA-zZ]{2,20}@[aA-zZ]{2,20}.[aA-zZ]{2,3}";
+            Regex regex = new Regex($Regex);
+            // Получаем совпадения
+            MatchCollection matches = regex.Matches(Value);
+            return matches.Count > 0;
+        }
+
+        private void tbPreviewNumber(object sender, TextCompositionEventArgs e)
+        {
+            e.Handled = !(Char.IsDigit(e.Text, 0));
+        }
+    }
+}
--- Pages/Manufacturer/Elements/Manufacturer.xaml.cs
+++ Pages/Manufacturer/Elements/Manufacturer.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using VinylRecordsApplication.Classes;
 
 namespace VinylRecordsApplication.Pages.Manufacturer.Elements
 {
@@ -20,9 +21,41 @@ namespace VinylRecordsApplication.Pages.Manufacturer.Elements
     /// </summary>
     public partial class Manufacturer : UserControl
     {
-        public Manufacturer()
+        IEnumerable<Classes.Country> Countries = Country.AllCountries();
+        Pages.Manufacturer.Main main;
+        Classes.Manufacturer manufacturer;
+
+        public Manufacturer(Classes.Manufacturer manufacturer, Pages.Manufacturer.Main main)
         {
             InitializeComponent();
+            tbName.Text = manufacturer.Name;
+            tbCountry.Text = Countries.Where(x => x.Id == manufacturer.CountryCode).First().Name;
+            tbPhone.Text = manufacturer.Phone.ToString();
+            tbEmail.Text = manufacturer.Mail;
+            this.manufacturer = manufacturer;
+            this.main = main;
+        }
+
+        private void EditManufacturer(object sender, RoutedEventArgs e)
+        {
+            MainWindow.mainWindow.OpenPage(new Pages.Manufacturer.Add(this.manufacturer));
+        }
+
+        private void DeleteManufacturer(object sender, RoutedEventArgs e)
+        {
+            if (MessageBox.Show($"Удалить поставщика: {this.manufacturer.Name}?", "Уведомление", MessageBoxButton.YesNo) == MessageBoxResult.Yes)
+            {
+                if (Classes.Record.AllRecords().Where(x => x.IdManufacturer == manufacturer.Id).Count() > 0)
+                {
+                    MessageBox.Show($"Поставщик {this.manufacturer.Name} невозможно удалить. Для начала удалите зависимости.", "Уведомление");
+                }
+                else
+                {
+                    this.manufacturer.Delete();
+                    main.manufacturerParent.Children.Remove(this);
+                    MessageBox.Show($"Поставщик {this.manufacturer.Name} успешно удалена.", "Уведомление");
+                }
+            }
         }
     }
 }
